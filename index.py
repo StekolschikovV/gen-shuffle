953ad7475261bb0eb54b52aef3979f112d3506cd
mk <v.a.stekolschikov@gmail.com>
2021-08-11 23:04:21 +0300

    add v3

---
+++ index.py
@@ -0,0 +1,4 @@
+import itertools
+# print list(itertools.permutations(["a","a","a","a","a","a","a","a", "b","b","b","b","b","b","b","b"], 8))
+# print set(list(itertools.permutations(["a","a","a","a","a","a","a","a", "b","b","b","b","b","b","b","b"], 8)))
+print len(list(itertools.permutations(["a","a","a","a","a","a","a","a", "b","b","b","b","b","b","b","b"], 8)))
