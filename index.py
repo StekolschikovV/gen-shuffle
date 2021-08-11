52aa305bb32b30801998444cb3ebfa93db0cfdc0
mk <v.a.stekolschikov@gmail.com>
2021-08-11 23:10:55 +0300

    v3 2

--- index.py
+++ index.py
@@ -1,4 +1,11 @@
 import itertools
+from datetime import datetime
+import time
+
+start_time = datetime.now()
+
 # print list(itertools.permutations(["a","a","a","a","a","a","a","a", "b","b","b","b","b","b","b","b"], 8))
 # print set(list(itertools.permutations(["a","a","a","a","a","a","a","a", "b","b","b","b","b","b","b","b"], 8)))
 print len(list(itertools.permutations(["a","a","a","a","a","a","a","a", "b","b","b","b","b","b","b","b"], 8)))
+
+print(datetime.now() - start_time)
