6512a647426c4a2c1e23ea7932629120b2ed1ee1
mk <v.a.stekolschikov@gmail.com>
2021-08-11 15:22:52 +0300

    v2

--- index.ts
+++ index.ts
@@ -1,19 +1,55 @@
-const gen1 = "AAAAAAAA"
-const gen2 = "BBBBBBBB"
+let t0
+let t1
+let t2
+let t3
+const v1 = () => {
+    t0 =  Date.now()
+    const gen1 = "AAAAAAAA";
+    const gen2 = "BBBBBBBB";
 
-let result = new Map();
+    let result = new Map();
 
-const shuffle = (arr) => {
-    return arr.sort(() => Math.round(Math.random() * 100) - 50);
+    const shuffle = (arr) => {
+        return arr.sort(() => Math.round(Math.random() * 100) - 50);
+    }
+
+    while (result.size < 256) {
+        const r = shuffle([...Array.from(gen1), ...Array.from(gen2)]).join('').slice(0, 8)
+        result.set(r, r)
+    }
+
+    t1 = Date.now()
+    return result
 }
 
-const t0 = +new Date()
+const v2 = () => {
+    t2 =  Date.now()
 
-while (result.size < 256) {
-    const r = shuffle([...Array.from(gen1), ...Array.from(gen2)]).join('').slice(0, 8)
-    result.set(r, r)
+    const gen1 = "AAAAAAAA";
+    const gen2 = "BBBBBBBB";
+
+    const zip = (arr, ...arrs) =>
+        arr.map((val, i) => arrs.reduce((a, arr) => [...a, arr[i]], [val]));
+
+    const product = (...arrays) => {
+        return [...arrays].reduce((a, b) =>
+            a.map(x => b.map(y => x.concat(y)))
+                .reduce((a, b) => a.concat(b), []), [[]]);
+    }
+
+    const zipped = zip(Array.from(gen1), Array.from(gen2));
+    const cartesianProductRes = product(...zipped)
+    let result = new Map();
+    cartesianProductRes.map( e => {
+        const key = e.join('')
+        result.set(key, key)
+    } )
+    t3 =  Date.now()
+
+    return result
 }
 
-const t1 = +new Date()
+console.log(v1().size, t1 - t0)
+console.log(v2().size, t3  - t2)
 
-console.log(result.size, t1 - t0)
+
